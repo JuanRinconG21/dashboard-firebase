9e888d281a377be6958e62f17ca441e85623ac6c
Jota <jotacodigos@gmail.com>
2024-05-06 15:59:59 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./assets/lib/owlcarousel/assets/owl.carousel.min.css";
+import "./assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css";
+function App() {
+  return <></>;
+}
+
+export default App;
